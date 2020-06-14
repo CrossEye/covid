f8ead470e5baedb1b335887894dcf146371ba478
Scott Sauyet <scott@sauyet.com>
2020-06-14 15:47:35 -0400

    Adding population information

--- all.js
+++ all.js
@@ -9,7 +9,8 @@ const pluck = (name) => (xs) =>
 const uniq = (xs) => 
   [...new Set(xs)]
 
-const diffs = (xs) => xs .map ((x, i) => i > 0 ? x - xs [i - 1] : x)
+const diffs = (xs) => 
+  xs .map ((x, i) => i > 0 ? x - xs [i - 1] : x)
 
 const sum = (ns) =>
   ns .reduce ((a, b) => a + b, 0)
@@ -150,17 +151,19 @@ const makeSparkline = (allDays) => {
   }
 }
 
-const addNational = (allDays) => {
+const addNational = (populations) => (allDays) => {
   const states = 
     Object .entries (groupBy (s => s.state) (allDays)) .map (([state, days]) => [state, {
       cases: {
         total: days .slice (-1) [0] .cases,
+        per100k: round(days .slice (-1) [0] .cases / populations[state] * 100000),
         oneDay: days .slice (-1) [0] .cases - days .slice (-2, -1) [0] .cases,
         sevenDays: days.slice (-1) [0] .cases - days .slice (-8, -7) [0] .cases,
         thirtyDays: days.slice (-1) [0] .cases - days .slice (-31, -1) [0] .cases,
       },
       deaths: {
         total: days .slice (-1) [0] .deaths,
+        per100k: round(days .slice (-1) [0] .deaths / populations[state] * 100000),
         oneDay: days .slice (-1) [0] .deaths - days .slice (-2, -1) [0] .deaths,
         sevenDays: days.slice (-1) [0] .deaths - days .slice (-8, -7) [0] .deaths,
         thirtyDays: days.slice (-1) [0] .deaths - days .slice (-31, -1) [0] .deaths,
@@ -175,27 +178,27 @@ const addNational = (allDays) => {
   national.innerHTML = `
   <table>
   <thead>
-  <tr><th></th><th colspan="5">Cases</th><th colspan="5">Deaths</th></tr>
-  <tr><th></th><th colspan="3">in previous...</th><th colspan="2"></th><th colspan="3">in previous...</th><th colspan="2"></th></tr>
+  <tr><th></th><th colspan="6">Cases</th><th colspan="6">Deaths</th></tr>
+  <tr><th></th><th colspan="3">in previous...</th><th colspan="3"></th><th colspan="3">in previous...</th><th colspan="3"></th></tr>
   <tr>
     <th>State</th>
-    <th>Day</th><th>Week</th><th>Month</th><th>Total</th><th>Trend</th>
-    <th>Day</th><th>Week</th><th>Month</th><th>Total</th><th>Trend</th>
+    <th>Day</th><th>Week</th><th>Month</th><th>Total</th><th>Per 100K</th><th>Trend</th>
+    <th>Day</th><th>Week</th><th>Month</th><th>Total</th><th>Per 100K</th><th>Trend</th>
   </tr>
   </thead>
     <tbody>
       ${byState.map(([state, {cases, deaths}]) =>
       `<tr>
         <td><a href="#/${ state .replace(/ /g, '+') }">${ state }</a></td>
-        <td>${cases.oneDay}</td><td>${cases.sevenDays}</td><td>${cases.thirtyDays}</td><td>${cases.total}</td><td>${casesSparkline (state)}</td>
-        <td>${deaths.oneDay}</td><td>${deaths.sevenDays}</td><td>${deaths.thirtyDays}</td><td>${deaths.total}</td><td>${deathsSparkline (state)}</td>
+        <td>${cases.oneDay}</td><td>${cases.sevenDays}</td><td>${cases.thirtyDays}</td><td>${cases.total}</td><td>${cases.per100k}</td><td>${casesSparkline (state)}</td>
+        <td>${deaths.oneDay}</td><td>${deaths.sevenDays}</td><td>${deaths.thirtyDays}</td><td>${deaths.total}</td><td>${deaths.per100k}</td><td>${deathsSparkline (state)}</td>
       </tr>`).join('\n      ')}
     </tbody>
     <tfoot>
       <tr>
         <th><a href="#/United+States">United States</a></th>
-        <th>${totals.cases.oneDay}</th><th>${totals.cases.sevenDays}</th><th>${totals.cases.thirtyDays}</th><th>${totals.cases.total}</th><th>${makeSparkline (allDays) ('cases', 100, 30, 'white') ('United States')}</th>
-        <th>${totals.deaths.oneDay}</th><th>${totals.deaths.sevenDays}</th><th>${totals.deaths.thirtyDays}</th><th>${totals.deaths.total}</th><th>${makeSparkline (allDays) ('deaths', 100, 30, 'white') ('United States')}</th>
+        <th>${totals.cases.oneDay}</th><th>${totals.cases.sevenDays}</th><th>${totals.cases.thirtyDays}</th><th>${totals.cases.total}</th><th>${totals.cases.per100k}</th><th>${makeSparkline (allDays) ('cases', 100, 30, 'white') ('United States')}</th>
+        <th>${totals.deaths.oneDay}</th><th>${totals.deaths.sevenDays}</th><th>${totals.deaths.thirtyDays}</th><th>${totals.deaths.total}</th><th>${totals.deaths.per100k}</th><th>${makeSparkline (allDays) ('deaths', 100, 30, 'white') ('United States')}</th>
       </tr>
     </tfoot>
   </table>`
@@ -214,7 +217,7 @@ const makeCharts = ({state, days}) => {
   )
 }
 
-const stateChooser = (days) => {
+const stateChooser = (populations) => (days) => {
   const states = uniq (pluck ('state') (days)) .sort()
   const details =  document .getElementById ('chooser')
   const national = document .getElementById ('show-national')
@@ -249,10 +252,20 @@ const stateChooser = (days) => {
 const displayError = (err) =>
   document .getElementById ('title') .innerHTML = `Problem loading data: ${err}`
 
+const populations = ((pops) => ({
+  ...pops,
+  "United States": sum (Object .values (pops))
+})) ({
+// from https://api.census.gov/data/2019/pep/population?get=POP,NAME&for=state:*
+// with additional territory information from http://en.wikipedia.com/wiki
+  ...({"Alabama": 4903185, "Alaska": 731545, "Arizona": 7278717, "Arkansas": 3017804, "California": 39512223, "Colorado": 5758736, "Connecticut": 3565287, "Delaware": 973764, "District of Columbia": 705749, "Florida": 21477737, "Georgia": 10617423, "Hawaii": 1415872, "Idaho": 1787065, "Illinois": 12671821, "Indiana": 6732219, "Iowa": 3155070, "Kansas": 2913314, "Kentucky": 4467673, "Louisiana": 4648794, "Maine": 1344212, "Maryland": 6045680, "Massachusetts": 6892503, "Michigan": 9986857, "Minnesota": 5639632, "Mississippi": 2976149, "Missouri": 6137428, "Montana": 1068778, "Nebraska": 1934408, "Nevada": 3080156, "New Hampshire": 1359711, "New Jersey": 8882190, "New Mexico": 2096829, "New York": 19453561, "North Carolina": 10488084, "North Dakota": 762062, "Ohio": 11689100, "Oklahoma": 3956971, "Oregon": 4217737, "Pennsylvania": 12801989, "Puerto Rico": 3193694, "Rhode Island": 1059361, "South Carolina": 5148714, "South Dakota": 884659, "Tennessee": 6829174, "Texas": 28995881, "Utah": 3205958, "Vermont": 623989, "Virginia": 8535519, "Washington": 7614893, "West Virginia": 1792147, "Wisconsin": 5822434, "Wyoming": 578759}),
+  ...({Guam: 168485, "Northern Mariana Islands": 56882, "Virgin Islands": 106977})
+})
+
 fetch ('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv')
   .then (r => r .text ())
   .then (csv2arr)
   .then (addUnitedStates)
-  .then (tap (addNational))
-  .then (tap (stateChooser))
+  .then (tap (addNational(populations)))
+  .then (tap (stateChooser(populations)))
   .catch (displayError)
